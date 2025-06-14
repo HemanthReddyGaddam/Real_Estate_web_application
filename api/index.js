2f7349dd1795229bc6b565d695f0e793d09e6695
HemanthReddyGaddam <f20212904@hyderabad.bits-pilani.ac.in>
2025-06-14 20:29:24 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB).then(()=>{
+    console.log("connected to mongodb");
+}).catch((err)=>{
+    console.log(err);
+});
 
 const app = express();
 
 app.listen(3000,()=>{
-    console.log("hii");
+    console.log("server running on port 3000");
 });
